61021efb8880e3a76c77e1fd79ef569c144be0de
FaridC <48392059+FaridC@users.noreply.github.com>
2019-06-03 00:48:02 -0500

    Enviar correo para crear evento

--- app/src/main/java/com/example/proyecto_final/EventosFragment.java
+++ app/src/main/java/com/example/proyecto_final/EventosFragment.java
@@ -1,11 +1,22 @@
 package com.example.proyecto_final;
 
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -13,6 +24,7 @@ import android.view.ViewGroup;
  */
 public class EventosFragment extends Fragment {
 
+    FloatingActionButton fab;
 
     public EventosFragment() {
         // Required empty public constructor
@@ -20,10 +32,87 @@ public class EventosFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eventos, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_eventos, container, false);
+
+        FloatingActionButton fab = rootView.findViewById(R.id.sendEvent);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNoteDialog();
+            }
+        });
+
+        return rootView;
     }
+
+    private void showNoteDialog(){
+        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(getContext());
+        View view = layoutInflaterAndroid.inflate(R.layout.event_dialog,  null);
+
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(getContext());
+        alertDialogBuilderUserInput.setView(view);
+
+        final EditText nomEvent = view.findViewById(R.id.nom);
+        final EditText lugar = view.findViewById(R.id.lugar);
+        final EditText categoria = view.findViewById(R.id.categoria);
+        final EditText fecha_hora = view.findViewById(R.id.fecha_hora);
+        final EditText desc = view.findViewById(R.id.desc);
+        TextView dialogTitle = view.findViewById(R.id.dialog_title);
+        dialogTitle.setText(getString(R.string.lbl_new_note_title));
+
+        alertDialogBuilderUserInput.setCancelable(false)
+                .setPositiveButton("Enviar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogBox, int id) {
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogBox, int id) {
+                dialogBox.cancel();
+            }
+        });
+
+        final AlertDialog alertDialog = alertDialogBuilderUserInput.create();
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //Mostrar mensaje cuando no se ingresa texto
+                if (TextUtils.isEmpty(nomEvent.getText().toString()) || TextUtils.isEmpty(lugar.getText().toString()) || TextUtils.isEmpty(categoria.getText().toString()) || TextUtils.isEmpty(fecha_hora.getText().toString()) || TextUtils.isEmpty(desc.getText().toString())){
+                    Toast.makeText(getContext(), "Campo Vacío!", Toast.LENGTH_SHORT).show();
+                    return;
+                }else{
+                    final String msg = "Nombre: "+nomEvent.getText().toString()
+                            +"\nCategoria: "+categoria.getText().toString()
+                            +"\nLugar: "+lugar.getText().toString()
+                            +"\nFecha y hora: "+fecha_hora.getText().toString()
+                            +"\nDescripción: "+desc.getText().toString();
+
+                    final String[] destinatarios = {"santosflota8@gmail.com"};
+
+                    sendEmail(destinatarios, "Deseo crear un evento", msg);
+                    alertDialog.dismiss();
+                }
+
+            }
+        });
+    }
+
+    public void sendEmail(String[] addresses, String subject, String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        try {
+            startActivity(Intent.createChooser(intent, "Enviar E-Mail"));
+        }catch (android.content.ActivityNotFoundException ex){
+            Toast.makeText(getContext(), "No se encontro aplicación de correo", Toast.LENGTH_LONG).show();
+        }
+    }
+
 
 }
