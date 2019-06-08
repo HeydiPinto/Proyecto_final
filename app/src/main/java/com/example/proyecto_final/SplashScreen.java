c6c0e3a20798cfe1e3c50f265e5940448f912c84
FaridC <48392059+FaridC@users.noreply.github.com>
2019-06-08 16:14:56 -0500

    Modificaciones

---
+++ app/src/main/java/com/example/proyecto_final/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.proyecto_final;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int DURACION_SPLASH = 4000;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()){
+                    Intent i = new Intent(SplashScreen.this, ActivityPrincipal.class);
+                    Toast.makeText(getApplicationContext(), "Conexión Estable", Toast.LENGTH_SHORT).show();
+                    startActivity(i);
+                    finish();
+                }else{
+                    Toast.makeText(getApplicationContext(), "No hay conexión a Internet\n       Prueba más tarde", Toast.LENGTH_SHORT).show();
+                }
+            finish();}
+        }, DURACION_SPLASH);
+    }
+}
