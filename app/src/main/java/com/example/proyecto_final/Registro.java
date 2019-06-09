9a16e0c6ae93b8d5bf969e09f79889c5495d84fa
FaridC <48392059+FaridC@users.noreply.github.com>
2019-06-09 17:25:50 -0500

    Modificacion Regitro xml y java

--- app/src/main/java/com/example/proyecto_final/Registro.java
+++ app/src/main/java/com/example/proyecto_final/Registro.java
@@ -3,6 +3,7 @@ package com.example.proyecto_final;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -45,29 +46,34 @@ public class Registro extends AppCompatActivity {
         });
     }
     public void enviarDatos(){
-        String url="http://puntosingular.mx/nave/ingresar.php";
-        StringRequest request= new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Toast.makeText(getApplicationContext(), "Operación exitosa", Toast.LENGTH_SHORT).show();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
-            }
-        }){ protected Map<String, String> getParams() throws AuthFailureError{
-            Map<String, String> map= new HashMap<>();
-            map.put("email",email.getText().toString());
-            map.put("nombre_usuario", nombre.getText().toString());
-            map.put("contra", contrasena.getText().toString());
-            map.put("fecha_nacimiento", fechaN.getText().toString());
-            return map;
-        }
 
-        };
-        rq.add(request);
-        Intent i = new Intent(getApplicationContext(), ActivityPrincipal.class);
-        startActivity(i);
+        if (TextUtils.isEmpty(email.getText().toString()) || TextUtils.isEmpty(nombre.getText().toString()) || TextUtils.isEmpty(contrasena.getText().toString()) || TextUtils.isEmpty(fechaN.getText().toString())){
+            Toast.makeText(getApplicationContext(), "Campos Vacíos!", Toast.LENGTH_SHORT).show();
+        }else{
+            String url="http://puntosingular.mx/nave/ingresar.php";
+            StringRequest request= new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Toast.makeText(getApplicationContext(), "Operación exitosa", Toast.LENGTH_SHORT).show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+                }
+            }){ protected Map<String, String> getParams() throws AuthFailureError{
+                Map<String, String> map= new HashMap<>();
+                map.put("email",email.getText().toString());
+                map.put("nombre_usuario", nombre.getText().toString());
+                map.put("contra", contrasena.getText().toString());
+                map.put("fecha_nacimiento", fechaN.getText().toString());
+                return map;
+            }
+
+            };
+            rq.add(request);
+            Intent i = new Intent(getApplicationContext(), ActivityPrincipal.class);
+            startActivity(i);
+        }
     }
 }
